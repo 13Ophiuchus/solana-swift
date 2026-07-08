e15d73006a1f1692a232e92a733b8b13ab659846
Nic Reich <ophiuchus@resourceless.org>
2026-07-08 05:22:22 -0400

    fix: repoint secp256k1 dependency to 21-DOT-DEV/swift-secp256k1 to match web3swift/AGWallet
    - Package.swift: swap GigaBitcoin/secp256k1.swift for 21-DOT-DEV/swift-secp256k1 (P256K)
    - CKSecp256k1.swift: import P256K + libsecp256k1 instead of bare secp256k1 module
    - Matches the dependency already used by AGWallet/Package.swift and web3swift-patched

--- Package.swift
+++ Package.swift
@@ -27,20 +27,19 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
-		.target(
-			name: "SolanaSwift",
-			dependencies: [
-				.product(name: "TweetNacl", package: "tweetnacl-swiftwrap"),
-				.product(name: "P256K", package: "swift-secp256k1"),
-				.product(name: "libsecp256k1", package: "swift-secp256k1"),
-				.product(name: "Task_retrying", package: "task-retrying-swift"),
-				.product(name: "Crypto", package: "swift-crypto"),
-			],
-			swiftSettings: [
-				.unsafeFlags(["-suppress-warnings"])
-			]
-		),
-
+        .target(
+            name: "SolanaSwift",
+            dependencies: [
+                .product(name: "TweetNacl", package: "tweetnacl-swiftwrap"),
+                .product(name: "P256K", package: "swift-secp256k1"),
+                .product(name: "libsecp256k1", package: "swift-secp256k1"),
+                .product(name: "Task_retrying", package: "task-retrying-swift"),
+                .product(name: "Crypto", package: "swift-crypto"),
+            ],
+            swiftSettings: [
+                .unsafeFlags(["-suppress-warnings"])
+            ]
+        ),
         .testTarget(
             name: "SolanaSwiftUnitTests",
             dependencies: ["SolanaSwift"],
